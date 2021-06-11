f88b6ed51ab28ed36d3da5b07c89009bdce651b5
lindx <linder.igor@gmail.com>
2021-06-12 02:54:24 +0300

    Task  03
    Написать метод, которому в качестве параметра передается целое число. Метод должен вернуть true, если число отрицательное, и вернуть false если положительное.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -10,6 +10,10 @@ public class App {
     System.out.printf("%2d - %s \n", 0, !isPositiveOrNegativeNumber(0) ? " положительное" : "отрицательное");
     System.out.printf("%2d - %s \n", -10, !isPositiveOrNegativeNumber(-10) ? " положительное" : "отрицательное");
     System.out.printf("%2d - %s \n", 5, !isPositiveOrNegativeNumber(5) ? " положительное" : "отрицательное");
+    
+    printPositiveOrNegativeNumber(-5);
+    printPositiveOrNegativeNumber(0);
+    printPositiveOrNegativeNumber(10);
   }
 
   public static boolean ariaSum(final int a, final int b) {
@@ -18,5 +22,9 @@ public class App {
 
   public static boolean isPositiveOrNegativeNumber(final int x) {
     return x < 0;
+  }
+
+  public static void printPositiveOrNegativeNumber(final int x) {
+    System.out.printf("%2d - %s \n", x, !isPositiveOrNegativeNumber(x) ? " положительное" : "отрицательное");
   }
 }
