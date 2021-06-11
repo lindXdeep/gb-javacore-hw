feaa680ed9439df08e3011eaa5f230d9807c15d4
lindx <linder.igor@gmail.com>
2021-06-11 03:17:24 +0300

    Сумма в методе
    Создайте метод checkSumSign(), в теле которого объявите две int переменные a и b, и инициализируйте их любыми значениями, которыми захотите. Далее метод должен просуммировать эти переменные, и если их сумма больше или равна 0, то вывести в консоль сообщение “Сумма положительная”, в противном случае - “Сумма отрицательная”;

--- src/main/java/HomeWorkApp.java
+++ src/main/java/HomeWorkApp.java
@@ -3,19 +3,29 @@
  */
 public class HomeWorkApp {
 
-  private static String[] words = {
-    "Orange",
-    "Banana",
-    "Apple"
-  };
+  private static String[] words = { "Orange", "Banana", "Apple" };
 
   public static void main(String[] args) {
     printThreeWords();
+    checkSumSign();
   }
 
-  public static void printThreeWords(){
+  public static void printThreeWords() {
     for (String str : words) {
       System.out.println(str);
+    }
+  }
+
+  public static void checkSumSign() {
+    int a = 1;
+    int b = 2;
+
+    int c  = a + b;
+
+    if (c >= 0) {
+      System.out.println("Сумма положительная");
+    }else{
+      System.out.println("Сумма отрицательная");
     }
   }
 }
