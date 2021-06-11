196b41d492ac69f0a98d445dd3d83554581311ea
lindx <linder.igor@gmail.com>
2021-06-12 02:47:00 +0300

    Task 02
    Написать метод, которому в качестве параметра передается целое число, метод должен напечатать в консоль, положительное ли число передали или отрицательное. Замечание: ноль считаем положительным числом.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -6,9 +6,17 @@ public class App {
   public static void main(String[] args) {
     System.out.println(ariaSum(15, 10));
     System.out.println(ariaSum(10, 10));
+
+    System.out.printf("%2d - %s \n", 0, !isPositiveOrNegativeNumber(0) ? " положительное" : "отрицательное");
+    System.out.printf("%2d - %s \n", -10, !isPositiveOrNegativeNumber(-10) ? " положительное" : "отрицательное");
+    System.out.printf("%2d - %s \n", 5, !isPositiveOrNegativeNumber(5) ? " положительное" : "отрицательное");
   }
 
   public static boolean ariaSum(final int a, final int b) {
     return (a + b) >= 10 & (a + b) <= 20;
+  }
+
+  public static boolean isPositiveOrNegativeNumber(final int x) {
+    return x < 0;
   }
 }
