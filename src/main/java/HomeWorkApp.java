b9575848233e306a0b4e43d6115f2d8f1703fe55
lindx <linder.igor@gmail.com>
2021-06-11 03:24:38 +0300

    task 04 printColor
    Создайте метод printColor() в теле которого задайте int переменную value и инициализируйте ее любым значением. Если value меньше 0 (0 включительно), то в консоль метод должен вывести сообщение “Красный”, если лежит в пределах от 0 (0 исключительно) до 100 (100 включительно), то “Желтый”, если больше 100 (100 исключительно) - “Зеленый”;

--- src/main/java/HomeWorkApp.java
+++ src/main/java/HomeWorkApp.java
@@ -8,6 +8,7 @@ public class HomeWorkApp {
   public static void main(String[] args) {
     printThreeWords();
     checkSumSign();
+    printColor();
   }
 
   public static void printThreeWords() {
@@ -26,6 +27,18 @@ public class HomeWorkApp {
       System.out.println("Сумма положительная");
     }else{
       System.out.println("Сумма отрицательная");
+    }
+  }
+
+  public static void printColor() {
+    int value = 100;
+
+    if (value <= 0) {
+      System.out.println("Красный");
+    }else if(value > 0 & value <= 100 ){
+      System.out.println("Желтый");
+    }else{
+      System.out.println("Зеленый");
     }
   }
 }
