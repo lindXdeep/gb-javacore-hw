0c9db4c5c176ca888d6fb333ba0aa0c5f4fb5ee5
lindx <linder.igor@gmail.com>
2021-07-15 21:30:46 +0300

    init home work 201-009

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,41 +1,6 @@
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.LinkedList;
-import java.util.List;
-
-/**
- * App
- */
 public class App {
 
-  public static Plate plate = new Plate(100);
-
-  public static Cat[] cats = {
-    new Cat("Cat1", 130),
-    new Cat("Cat2", 20),
-    new Cat("Cat3", 115),
-    new Cat("Cat4", 45),
-    new Cat("Cat5", 110),
-  };
-
   public static void main(String[] args) {
-
-    plate.setEat(350);
-
-    for (Cat cat : cats) {
-      System.out.printf("%s %s\n", cat.getName(), cat.isFedUp() ? " сыт" : " голоден" );
-    }
-
-    System.out.println("-----");
-
-    for (int i = 0; i < cats.length; i++) {
-      cats[i].eat(plate);
-    }
-
-    for (Cat cat : cats) {
-      System.out.printf("%s %s\n", cat.getName(), cat.isFedUp() ? " сыт" : " голоден" );
-    }
+    System.out.println("test");
   }
 }
--- src/main/java/Dog.java
+++
@@ -1,6 +0,0 @@
-public class Dog extends Animal {
-
-  public Dog(final String name, int run, int swim) {
-    super(name, run, swim);
-  }
-}
