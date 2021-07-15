b7694f47c03de8e86abb8177f486879108907ddd
lindx <linder.igor@gmail.com>
2021-07-15 21:17:35 +0300

    swing

--- src/main/java/Panel.java
+++ src/main/java/Panel.java
@@ -1,13 +1,14 @@
-import javax.swing.JButton;
-import javax.swing.JPanel;
-import java.awt.Color;
 import java.awt.Font;
+
+import javax.swing.JPanel;
 
 public class Panel extends JPanel {
 
-  Font fontScreen = new Font("Arial Narrow", Font.BOLD, 40);
-	Font fontResult = new Font("Arial Narrow", Font.BOLD, 50);
-	Font fontNums = new Font("Arial", Font.BOLD, 25);
+  private Font fontScreen = new Font("Arial Narrow", Font.BOLD, 40);
+	private Font fontResult = new Font("Arial Narrow", Font.BOLD, 50);
+	private Font fontNums = new Font("Arial", Font.BOLD, 25);
+
+  private char[][] field;
 
   final int SIZE;
   final int WIN;
