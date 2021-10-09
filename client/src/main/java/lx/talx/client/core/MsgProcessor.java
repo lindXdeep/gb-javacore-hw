ce993ccd23b8b1fb8759d84d3af5bd5d5851ed44
lindx <linder.igor@gmail.com>
2021-10-10 01:51:59 +0300

    Добавить в сетевой чат запись локальной истории в текстовый файл на клиенте

--- client/src/main/java/lx/talx/client/core/MsgProcessor.java
+++ client/src/main/java/lx/talx/client/core/MsgProcessor.java
@@ -16,6 +16,25 @@ import lx.talx.client.utils.Log;
 
 public class MsgProcessor implements IMessageProcessor {
 
+  // regex pattern recipient user
+  private Pattern pUser = Pattern.compile("^@[a-zA-Z]{0,64}\\s");
+  private Pattern pMsg = Pattern.compile("\\s.{0,4096}");
+  private Matcher m;
+
+  private Date date = new Date();
+
+  private Path root = Paths.get("");
+  private Path sptr = Paths.get(File.separator);
+  private Path db = Paths.get("db");
+  private Path db_root = Paths.get(new String(db.toString() + sptr));
+
+  public MsgProcessor() {
+
+    if (!checkDbDir()) {
+      Log.log("Unable to create database for messages");
+    }
+  }
+
   @Override
   public void processMessage(String recive) {
 
@@ -33,7 +52,7 @@ public class MsgProcessor implements IMessageProcessor {
       System.out.print("\n" + recive.substring(7) + "\n\n::>");
     } else if (recive.startsWith("/ping")) {
       // ignore
-    }else if (recive.startsWith("/whoami")) {
+    } else if (recive.startsWith("/whoami")) {
       JSONObject juser = (JSONObject) JSONValue.parse(recive.substring(7));
       System.out.println("\n  ID:       " + juser.get("id"));
       System.out.println("  Email:    " + juser.get("email"));
@@ -41,6 +60,87 @@ public class MsgProcessor implements IMessageProcessor {
       System.out.println("  Nickname: " + juser.get("nickname"));
       System.out.print("\n::>");
     }
+  }
+
+  @Override
+  public List<String> getMessages(int num, String user) {
+
+    Path p = Paths.get("db" + sptr + user);
+    List<String> msgs = new ArrayList<>();
+
+    if (Files.exists(p, LinkOption.NOFOLLOW_LINKS)) {
+
+      try (BufferedReader in = new BufferedReader(new InputStreamReader(new FileInputStream("db" + sptr + user)))) {
+
+        String tmp;
+        while ((tmp = in.readLine()) != null) {
+
+          msgs.add(tmp);
+          if (msgs.size() == num)
+            msgs.remove(0);
+        }
+
+        in.close();
+
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    }
+    return msgs;
+  }
+
+  @Override
+  public void writeFromSender(String recive) {
+    String user = null;
+    String message = null;
+    long time = date.getTime();
+
+    if ((m = pUser.matcher(recive)).find())
+      user = recive.substring(m.start(), m.end());
+
+    if ((m = pMsg.matcher(recive)).find())
+      message = recive.substring(m.start(), m.end()).trim();
+
+    write(user, time + " >>> " + message + "\n");
+  }
+
+  @Override
+  public void writeForRecipient(String user, String message) {
+
+    long time = date.getTime();
+
+    write(user, time + " <<< " + message + "\n");
+  }
+
+  private void write(String user, String msg) {
+
+    try (DataOutputStream fout = new DataOutputStream(new FileOutputStream("db" + sptr + user.trim(), true))) {
+      fout.write(msg.getBytes());
+      fout.flush();
+      fout.close();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  private boolean checkDbDir() {
+    if (Files.isReadable(root) && Files.isWritable(root)) {
+      if (!Files.exists(db, LinkOption.NOFOLLOW_LINKS)
+          || (Files.exists(db, LinkOption.NOFOLLOW_LINKS) && !Files.isDirectory(db, LinkOption.NOFOLLOW_LINKS))) {
+        try {
+          Files.createDirectory(db);
+          return true;
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      }
+      return true;
+    }
+
+    Log.log("No permission to write in the current program directory");
+    System.exit(0);
+
+    return false;
   }
 
   private void showOnline(String jsonArr) {
@@ -61,23 +161,5 @@ public class MsgProcessor implements IMessageProcessor {
     }
 
     System.out.print("\n::>");
-  }
-
-  @Override
-  public List<String> getMessages(int num, String user) {
-    // TODO Auto-generated method stub
-    return null;
-  }
-
-  @Override
-  public void writeFromSender(String recive) {
-    // TODO Auto-generated method stub
-    
-  }
-
-  @Override
-  public void writeForRecipient(String user, String message) {
-    // TODO Auto-generated method stub
-    
   }
 }
