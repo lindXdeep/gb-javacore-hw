85922f26afd725f34b231e83a3027fc56af0be78
lindx <linder.igor@gmail.com>
2021-10-23 22:32:49 +0300

    init game

---
+++ src/main/java/Car.java
@@ -0,0 +1,42 @@
+public class Car implements Runnable {
+
+  private static int CARS_COUNT;
+
+  static {
+    CARS_COUNT = 0;
+  }
+
+  private Race race;
+  private int speed;
+  private String name;
+
+  public Car(Race race, int speed) {
+    this.race = race;
+    this.speed = speed;
+    this.name = "Участник #" + ++CARS_COUNT;
+  }
+
+  public int getSpeed() {
+    return this.speed;
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+  @Override
+  public void run() {
+    try {
+      System.out.println(this.name + " готовится");
+      Thread.sleep(500 + (int) (Math.random() * 800));
+      System.out.println(this.name + " готов");
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+
+    for (int i = 0; i < race.getStages().size(); i++) {
+      race.getStages().get(i).go(this);
+    }
+
+  }
+}
---
+++ src/main/java/Race.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Race {
+
+  private ArrayList<Stage> stages;
+
+  public Race(Stage... stages) {
+    this.stages = new ArrayList<>(Arrays.asList(stages));
+  }
+
+  public ArrayList<Stage> getStages() {
+    return stages;
+  }
+}
