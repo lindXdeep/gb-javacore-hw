1397f65f19da677d6351b7ff6f7e3a3242754b35
lindx <linder.igor@gmail.com>
2021-10-24 06:54:27 +0300

    Урок 5. Многопоточность. Часть II

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,9 +1,13 @@
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CyclicBarrier;
 
 public class App {
   
   public static final int CARS_COUNT = 4;
 
+  public static int WINNER = 1;
+  public static final CyclicBarrier CD_BARRIER = new CyclicBarrier(CARS_COUNT);
+  public static final CountDownLatch CDL_START = new CountDownLatch(CARS_COUNT);
   public static final CountDownLatch CDL_FINISH = new CountDownLatch(CARS_COUNT);
 
   public static void main(String[] args) throws InterruptedException {
@@ -26,14 +30,12 @@ public class App {
       cars[i] = new Car(race, 20 + (int) (Math.random() * 10));
     }
 
-    System.out.println("Start");
-
     for (int i = 0; i < cars.length; i++) {
       new Thread(cars[i]).start();
     }
 
+    CDL_START.await();
     System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
-
 
     CDL_FINISH.await();
     System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
--- src/main/java/Tunnel.java
+++ src/main/java/Tunnel.java
@@ -1,4 +1,8 @@
+import java.util.concurrent.Semaphore;
+
 public class Tunnel extends Stage {
+
+  private Semaphore limit = new Semaphore(App.CARS_COUNT / 2);
 
   public Tunnel() {
     this.length = 80;
@@ -9,12 +13,18 @@ public class Tunnel extends Stage {
   public void go(Car c) {
     try {
       try {
+
+        limit.acquire();
+
         System.out.println(c.getName() + " готовится к этапу(ждет): " + description);
         System.out.println(c.getName() + " начал этап: " + description);
         Thread.sleep(length / c.getSpeed() * 1000);
       } catch (InterruptedException e) {
         e.printStackTrace();
       } finally {
+
+        limit.release();
+        
         System.out.println(c.getName() + " закончил этап: " + description);
       }
     } catch (Exception e) {
