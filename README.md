ecf60ff1166a45d22ff6bc02c10dc4ba618b4595
lindx <linder.igor@gmail.com>
2021-10-28 13:34:32 +0300

    Написать метод, которому в качестве аргумента передается не пустой одномерный целочисленный массив.

--- README.md
+++ README.md
@@ -12,61 +12,14 @@
 Вх: [ 1 2 4 4 2 3 4 1 7 ] -> вых: [ 1 7 ].
 ```
 
-2. Написать метод, который проверяет состав массива из чисел 1 и 4. Если в нем нет хоть одной четверки или единицы, то метод вернет false; Написать набор тестов для этого метода (по 3-4 варианта входных данных).
+2. Написать метод, который проверяет состав массива из чисел 1 и 4. 
+
+   Если в нем нет хоть одной четверки или единицы, то метод вернет false; Написать набор тестов для этого метода (по 3-4 варианта входных данных).
 
 ```
 [ 1 1 1 4 4 1 4 4 ] -> true
 [ 1 1 1 1 1 1 ] -> false
 [ 4 4 4 4 ] -> false
 [ 1 4 4 1 1 4 3 ] -> false
-```
-
-```
-import java.util.HashMap;
-import java.util.Map;
-
-public class App {
-  public static void main(String[] args) {
-
-    int[] arr = new int[] { 1, 2, 4, 4, 2, 3, 4, 1, 7 };
-
-    App app = new App();
-
-    System.out.println("--------");
-    int[] a = app.newAfterFore(arr);
-    for (int i : a) {
-      System.out.print(i + " ");
-    }
-
-  }
-
-  public int[] newAfterFore(final int[] arr) {
-
-    boolean startFill = false;
-    int i = 0;
-    int s = 0;
-    int j = 0;
-
-    while (i++ < arr.length) {
-      if (arr[i] == 4) {
-        
-        s = arr.length - i;
-        j = arr.length - s;
-
-        if (s != 0) {
-
-          int[] newArr = new int[s];
-
-          while (i < arr.length) {
-            newArr[i - j] = arr[i++];
-          }
-          return newArr;
-        }
-      }
-    }
-
-    throw new RuntimeException();
-  }
-}
 ```
 
--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,5 +1,7 @@
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -12,7 +14,7 @@ import org.junit.jupiter.api.Test;
 public class AppTest {
 
   private static App app;
-  private static int[] test1, test2, test3;
+  private static int[] test1, test2, test3, test4_1, test4_2, test4_3, test4_4;
 
   private static Class<RuntimeException> test1_expected;
   private static int[] test2_expected;
@@ -25,6 +27,11 @@ public class AppTest {
 
     test1_expected = RuntimeException.class;
     test2_expected = new int[] { 1, 7 };
+
+    test4_1 = new int[] { 1, 1, 1, 4, 4, 1, 4, 4 }; // true
+    test4_2 = new int[] { 1, 1, 1, 1, 1, 1 }; // false
+    test4_3 = new int[] { 4, 4, 4, 4 }; // false
+    test4_4 = new int[] { 1, 4, 4, 1, 1, 4, 3 }; // false
   }
 
   @BeforeEach
@@ -48,6 +55,31 @@ public class AppTest {
   @DisplayName("Тестируем на пустой массив")
   void test3() {
     assertThrows(test1_expected, () -> app.newAfterFore(test3));
+  }
+
+  @Test
+  @DisplayName("Тестируем в штатном режиме.")
+  void test4_1() {
+    assertTrue(app.containForeAndOne(test4_1));
+  }
+
+  @Test
+  @DisplayName("Тестируем на все единицы.")
+  void test4_2() {
+    assertFalse(app.containForeAndOne(test4_2));
+
+  }
+
+  @Test
+  @DisplayName("Тестируем на все четверки.")
+  void test4_3() {
+    assertFalse(app.containForeAndOne(test4_3));
+  }
+
+  @Test
+  @DisplayName("Тестируем на лишняя цифра")
+  void test4_4() {
+    assertFalse(app.containForeAndOne(test4_4));
   }
 
 }
